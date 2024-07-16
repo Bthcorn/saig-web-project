b37e29533dc78eda0caccefa2ecd13ee746428a5
Bthcorn <bowornthat.maize@gmail.com>
2024-07-16 09:46:12 +0000

    test connect prisma with mongodb

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "mysample@gmail.com",
+    },
+  });
+
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
