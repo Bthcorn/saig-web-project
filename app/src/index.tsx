7c96c9945848b488e7cc02456264dbe745355b71
Bthcorn <bowornthat.maize@gmail.com>
2024-07-30 18:41:20 +0000

    install ui components

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
 
